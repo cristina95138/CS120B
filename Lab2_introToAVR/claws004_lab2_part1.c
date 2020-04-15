d8039aeaadd7ebf896766c0c89afcbe64f63222a
Cristina Lawson <lawson.cristina@gmail.com>
2020-04-14 19:12:18 -0700

    Update claws004_lab2_part1.c

--- Lab2_introToAVR/claws004_lab2_part1.c
+++ Lab2_introToAVR/claws004_lab2_part1.c
@@ -1,34 +1,3 @@
-/*	Author: cristinalawson
- *  Partner(s) Name: 
- *	Lab Section: 021
- *	Assignment: Lab #2  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
- *
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
-
-int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
-							// Initialize output on PORTB to 0x00
-	unsigned char PAZERO = 0x00;
-	unsigned char PAONE = 0x00;
-	
-	while(1) {
-		PAZERO = PINA & 0x01;
-		PAONE = PINA & 0x02;
-
-		if((PAZERO == 0x01) && (PAONE == 0x00)) {
-			PORTB = 0x01;
-		}
-		else {
-			PORTB = 0x00;
-		}
-	}
-	return 0;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:31064b8da2d8ab468fbeedb92ba67c580bd8cb1804cc471129f7d7148a9ffbe0
+size 789
